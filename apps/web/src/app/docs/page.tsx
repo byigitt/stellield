4b7da7ede4dc1b9f014ddc2768043f2fc1f1026e
barış <137426514+byigitt@users.noreply.github.com>
2025-11-02 13:41:30 +0300

    fix: docs redirect

--- apps/web/src/app/docs/page.tsx
+++ apps/web/src/app/docs/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export default function DocsRedirect() {
 	useEffect(() => {
-		window.location.replace(process.env.NODE_ENV === "production" ? "https://docs.stellield.baris.world" : "http://localhost:3002/docs");
+		window.location.replace(process.env.NODE_ENV === "production" ? "https://docs-stellield.baris.world" : "http://localhost:3002/docs");
 	}, []);
 
 	return null;
